2976e9702be54e8160ed42452fc7783cf12b34a3
DanielGarQ <danielgarciaquiceno@gmail.com>
2025-05-08 14:36:05 -0500

    Carga inicial backen sibe

---
+++ src/main/java/co/edu/uco/sibe/SibeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.sibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SibeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uco/sibe/SibeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.sibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
