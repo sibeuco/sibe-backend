1f738e7ee3659211aa8d1c18f1b2c003c57cd550
DanielGarQ <danielgarciaquiceno@gmail.com>
2025-07-14 18:45:41 -0500

    Se corrigen errores de seguridad.

--- src/main/java/co/edu/uco/sibe/infraestructura/adaptador/dao/UsuarioDAO.java
+++ src/main/java/co/edu/uco/sibe/infraestructura/adaptador/dao/UsuarioDAO.java
@@ -1,11 +1,14 @@
 package co.edu.uco.sibe.infraestructura.adaptador.dao;
 
+import co.edu.uco.sibe.dominio.dto.UsuarioDTO;
 import co.edu.uco.sibe.infraestructura.adaptador.entidad.UsuarioEntidad;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UsuarioDAO extends JpaRepository<UsuarioEntidad, UUID> {
@@ -52,4 +55,6 @@ public interface UsuarioDAO extends JpaRepository<UsuarioEntidad, UUID> {
     WHERE identificador = :identificador
     """, nativeQuery = true)
     void cambiarContrasena(String nuevaContrasena, UUID identificador);
+
+
 }
