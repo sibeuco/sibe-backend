3f574fb996c1d08154db236aae92b18deab6d0fa
DanielGarQ <danielgarciaquiceno@gmail.com>
2025-10-13 08:51:01 -0500

    Se agregan comandos faltantes para la gestión de los participantes

---
+++ src/main/java/co/edu/uco/sibe/aplicacion/comando/DatosEmpleadoComando.java
@@ -0,0 +1,18 @@
+package co.edu.uco.sibe.aplicacion.comando;
+
+public class DatosEmpleadoComando {
+    private String nombre;
+    private String genero;
+    private String identificacion;
+    private String id_carnet;
+    private String codigo_clasificacion_7;
+    private String grupo_relacion_laboral;
+    private String codigo_centro_costos;
+    private String centro_costos_n1;
+    private String codigo_ciudad_residencia;
+    private String ciudad_de_residencia;
+
+
+
+
+}
