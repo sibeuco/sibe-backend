4137d6923e56c75b7821e3dc85149a7c4813d485
CEIBA\esteban.colorado <estecogn@suramericana.com.co>
2025-11-11 22:59:15 -0500

    refactor: se corrige devolucion del objeto participante externo

--- src/main/java/co/edu/uco/sibe/dominio/service/RegistrarParticipanteService.java
+++ src/main/java/co/edu/uco/sibe/dominio/service/RegistrarParticipanteService.java
@@ -3,7 +3,6 @@ package co.edu.uco.sibe.dominio.service;
 import co.edu.uco.sibe.dominio.modelo.Participante;
 import co.edu.uco.sibe.dominio.puerto.comando.ParticipanteRepositorioComando;
 import co.edu.uco.sibe.dominio.puerto.consulta.ParticipanteRepositorioConsulta;
-import java.util.UUID;
 import static co.edu.uco.sibe.dominio.transversal.utilitarios.ValidadorObjeto.esNulo;
 
 public class RegistrarParticipanteService {
@@ -19,18 +18,11 @@ public class RegistrarParticipanteService {
         var participanteExistente = participanteRepositorioConsulta.consultarPorDocumentoMiembro(participante.getMiembro().getNumeroIdentificacion());
 
         if (esNulo(participanteExistente)) {
-            var identificador = participanteRepositorioComando.guardar(participante);
+            participanteRepositorioComando.guardar(participante);
 
-            return obtenerParticipanteConId(participante, identificador);
+            return participante;
         }
 
         return participanteExistente;
-    }
-
-    private Participante obtenerParticipanteConId(Participante participante, UUID identificador) {
-        return Participante.construir(
-                identificador,
-                participante.getMiembro()
-        );
     }
 }
