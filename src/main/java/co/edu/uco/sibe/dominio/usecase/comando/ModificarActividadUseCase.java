b6020a8ef8b6e29b78be2dfe4886e316317d7838
CEIBA\esteban.colorado <estecogn@suramericana.com.co>
2025-11-13 00:39:05 -0500

    refactor: se agregan validaciones al modificar actividad

--- src/main/java/co/edu/uco/sibe/dominio/usecase/comando/ModificarActividadUseCase.java
+++ src/main/java/co/edu/uco/sibe/dominio/usecase/comando/ModificarActividadUseCase.java
@@ -9,10 +9,15 @@ import co.edu.uco.sibe.dominio.regla.TipoOperacion;
 import co.edu.uco.sibe.dominio.regla.fabrica.MotoresFabrica;
 import co.edu.uco.sibe.dominio.service.ModificarVinculacionActividadConAreaService;
 import co.edu.uco.sibe.dominio.transversal.excepcion.ValorDuplicadoExcepcion;
+import co.edu.uco.sibe.dominio.transversal.excepcion.ValorInvalidoExcepcion;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
-import static co.edu.uco.sibe.dominio.transversal.constante.MensajesErrorConstante.ACTIVIDAD_EXISTENTE_DURANTE_SEMESTRE_ACTUAL;
-import static co.edu.uco.sibe.dominio.transversal.constante.MensajesErrorConstante.ACTIVIDAD_NO_EXISTE_CON_IDENTIFICADOR;
+import static co.edu.uco.sibe.dominio.transversal.constante.MensajesErrorConstante.*;
+import static co.edu.uco.sibe.dominio.transversal.constante.MensajesErrorConstante.FECHA_PROGRAMADA_NO_PERTENECE_AL_SEMESTRE_DE_LA_ACTIVIDAD;
+import static co.edu.uco.sibe.dominio.transversal.constante.TextoConstante.NUMERO_CERO_DOS;
+import static co.edu.uco.sibe.dominio.transversal.constante.TextoConstante.NUMERO_CERO_UNO;
+import static co.edu.uco.sibe.dominio.transversal.utilitarios.UtilFecha.obtenerFechaActual;
 import static co.edu.uco.sibe.dominio.transversal.utilitarios.ValidadorObjeto.esNulo;
 
 public class ModificarActividadUseCase {
@@ -40,6 +45,9 @@ public class ModificarActividadUseCase {
         ejecucionesActividad.forEach(ejecucionActividad -> {
             MotoresFabrica.MOTOR_ESTADO_ACTIVIDAD.ejecutar(ejecucionActividad.getEstado(), TipoOperacion.CREAR);
             MotoresFabrica.MOTOR_EJECUCION_ACTIVIDAD.ejecutar(ejecucionActividad, TipoOperacion.CREAR);
+
+            validarSiFechaProgramadaEsAnteriorAFechaActual(ejecucionActividad.getFechaProgramada());
+            validarSiFechaProgramadaPerteneceASemestreDeLaActividad(ejecucionActividad.getFechaProgramada(), actividad.getSemestre());
         });
 
         actividadRepositorioComando.modificarEjecuciones(ejecucionesActividad);
@@ -58,5 +66,33 @@ public class ModificarActividadUseCase {
         if (!esNulo(actividad) && !actividad.getIdentificador().equals(identificador)){
             throw new ValorDuplicadoExcepcion(ACTIVIDAD_EXISTENTE_DURANTE_SEMESTRE_ACTUAL);
         }
+    }
+
+    private void validarSiFechaProgramadaEsAnteriorAFechaActual(LocalDate fechaProgramada) {
+        if(fechaProgramada.isBefore(obtenerFechaActual())) {
+            throw new ValorInvalidoExcepcion(FECHA_PROGRAMADA_NO_PUEDE_SER_ANTERIOR_A_FECHA_ACTUAL);
+        }
+    }
+
+    private void validarSiFechaProgramadaPerteneceASemestreDeLaActividad(LocalDate fechaProgramada, String semestre) {
+        var anioSemestre = Integer.parseInt(semestre.substring(0, 4));
+        var anioFecha = fechaProgramada.getYear();
+        var mesFecha = fechaProgramada.getMonthValue();
+
+        if (semestre.endsWith(NUMERO_CERO_UNO) && !validarPrimerSemestre(anioSemestre, anioFecha, mesFecha)) {
+            throw new ValorInvalidoExcepcion(FECHA_PROGRAMADA_NO_PERTENECE_AL_SEMESTRE_DE_LA_ACTIVIDAD);
+        }
+
+        if (semestre.endsWith(NUMERO_CERO_DOS) && !validarSegundoSemestre(anioSemestre, anioFecha, mesFecha)) {
+            throw new ValorInvalidoExcepcion(FECHA_PROGRAMADA_NO_PERTENECE_AL_SEMESTRE_DE_LA_ACTIVIDAD);
+        }
+    }
+
+    private static boolean validarPrimerSemestre(int anioSemestre, int anioFecha, int mesFecha) {
+        return anioSemestre == anioFecha && mesFecha <= 6;
+    }
+
+    private static boolean validarSegundoSemestre(int anioSemestre, int anioFecha, int mesFecha) {
+        return anioSemestre == anioFecha && mesFecha >= 7;
     }
 }
