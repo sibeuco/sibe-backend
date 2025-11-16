7f8f135345f520871a6cd5dadece20ef24f18969
CEIBA\esteban.colorado <estebancoloradogonzalez@gmail.com>
2025-11-16 18:20:16 -0500

    fix: se arregla formato de fechas

--- src/main/java/co/edu/uco/sibe/infraestructura/adaptador/mapeador/FilaExcelBaseMapeador.java
+++ src/main/java/co/edu/uco/sibe/infraestructura/adaptador/mapeador/FilaExcelBaseMapeador.java
@@ -1,24 +1,28 @@
 package co.edu.uco.sibe.infraestructura.adaptador.mapeador;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import static co.edu.uco.sibe.dominio.transversal.constante.TextoConstante.VACIO;
 
 public abstract class FilaExcelBaseMapeador<T> implements FilaExcelMapeador<T> {
     private final DataFormatter dataFormatter = new DataFormatter();
 
-    protected String obtenerValorCelda(Row fila, Integer indice) {
-        if (indice == null) {
-            return VACIO;
-        }
-
-        Cell celda = fila.getCell(indice);
+    protected String obtenerValorCelda(Row fila, int indiceColumna) {
+        Cell celda = fila.getCell(indiceColumna);
 
         if (celda == null) {
             return VACIO;
         }
 
-        return dataFormatter.formatCellValue(celda).trim();
+        if (celda.getCellType() == CellType.NUMERIC && DateUtil.isCellDateFormatted(celda)) {
+            Date fechaCelda = celda.getDateCellValue();
+
+            var sdf = new SimpleDateFormat("MM/dd/yyyy");
+            return sdf.format(fechaCelda);
+
+        } else {
+            return celda.toString();
+        }
     }
 }
